84be88442e9bb7877e08537229765316149b077f
Josias DeLaTorre <jozillasaurus@joes-mbp.home>
2020-10-12 06:31:46 -0700

    gl6

---
+++ what-is-in-your-fridge/src/Items.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function Items(props) {
+  const [item, setItem] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [purchaseDate, setPurchaseDate] = useState("");
+  const [expirationDate, setexpirationDate] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const fields = {
+      item,
+      quantity,
+      purchaseDate,
+      expirationDate,
+    };
+    const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/recipes`;
+    await axios.post(
+      airtableURL,
+      { fields },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+        },
+      }
+    );
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="">Item</label>
+      <input
+        type="text"
+        value={item}
+        onChange={(e) => setItem(e.target.value)}
+      />
+      <label htmlFor="">Quantity</label>
+      <input
+        type="text"
+        value={quantity}
+        onChange={(e) => setQuantity(e.target.value)}
+      />
+      <label htmlFor="">Purchase Date</label>
+      <input
+        type="text"
+        value={purchaseDate}
+        onChange={(e) => setPurchaseDate(e.target.value)}
+      />
+      <label htmlFor="">Expiration Date</label>
+      <input
+        type="text"
+        value={expirationDate}
+        onChange={(e) => setexpirationDate(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default Items;
