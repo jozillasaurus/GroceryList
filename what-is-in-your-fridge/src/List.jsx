d04f85433a4ef79cdb2314bf11b4a2b6caeca2dc
Josias DeLaTorre <jozillasaurus@joes-mbp.home>
2020-10-13 09:19:48 -0700

    delete-button

--- what-is-in-your-fridge/src/List.jsx
+++ what-is-in-your-fridge/src/List.jsx
@@ -1,12 +1,27 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 import { useParams } from "react-router-dom";
 
 function List(props) {
   const params = useParams();
-
   const thing = props.item.find((r) => r.id === params.id);
+  const [deleted, setDeleted] = useState(false);
 
+  const handleDelete = async (id) => {
+    setDeleted(true);
+    setTimeout(async () => {
+      // let's get the airtableURL...
+      const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/fridge/${props.thing.id}`;
+      // ...and make an axios delete request for a particular record
+      await axios.delete(airtableURL, {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+        },
+      });
+      props.List(!props.List);
+      setDeleted(false);
+    }, 2000);
+  };
   if (!thing) {
     return <h4>Loading...</h4>;
   }
@@ -17,6 +32,9 @@ function List(props) {
       <h4>Quantity:{thing.fields.quantity}</h4>
       <h5>Date of Purchase:{thing.fields.purchaseDate}</h5>
       <h6>Expiration Date:{thing.fields.expirationDate}</h6>
+      <button onClick={() => handleDelete(props.thing.id)}>
+        {deleted ? "Deleted" : "Delete"}
+      </button>
     </div>
   );
 }
